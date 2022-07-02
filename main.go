5412c0c07b24c1031786b6220f90e3afa7f805bc
alexfabianojr <59780094+alexfabianojr@users.noreply.github.com>
2022-07-02 12:37:58 -0300

    query param and path param

--- main.go
+++ main.go
@@ -12,9 +12,34 @@ func main() {
 
 	webClient := echo.New()
 
-	webClient.GET("/alive", func(context echo.Context) error {
-		return context.String(http.StatusOK, "I am alive!")
-	})
+	webClient.GET("/alive", handleAlive)
+
+	webClient.GET("/cats/:data", handleSearchCats)
 
 	webClient.Start(":8080")
+}
+
+func handleAlive(context echo.Context) error {
+	return context.String(http.StatusOK, "I am alive!")
+}
+
+func handleSearchCats(context echo.Context) error {
+	catName := context.QueryParam("name")
+	catType := context.QueryParam("type")
+	dataType := context.Param("data")
+
+	if dataType == "string" {
+		return context.String(http.StatusOK, fmt.Sprintf("Your cat name is %s and type is %s", catName, catType))
+	}
+
+	if dataType == "json" {
+		return context.JSON(http.StatusOK, map[string]string{
+			"name": catName,
+			"type": catType,
+		})
+	}
+
+	return context.JSON(http.StatusBadRequest, map[string]string{
+		"error": "You need to insert string or json data type",
+	})
 }
